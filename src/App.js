a732d91479c874f64a9ea64d74a65f1e1c30d6f6
Steven Emerson <code@stevenemerson.co.uk>
2024-03-20 20:22:22 +0000

    Added state and handleTodo function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React, { useState, useRef } from "react"
+import TodoList from "./TodoList"
+import { v4 as uuidv4 } from 'uuid'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
+    })
+    todoNameRef.current.value = null
+  }
+
+
   return (
-    <TodoList />
+    <>
+      <TodoList todos={todos}/>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
   )
 }
 
